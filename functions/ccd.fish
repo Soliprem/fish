171ef76dec015335105eb2d111fefa592621ce25
soliprem <franci.solidoro@gmail.com>
2023-06-01 21:43:19 +0200

    update

--- functions/ccd.fish
+++
@@ -1,7 +0,0 @@
-function ccd
-    if test -n "$argv"
-        cd "$HOME/.config/$argv" || echo error
-    else
-        cd "$HOME"/.config/(find "$HOME"/.config/*/ -type d -not -path "*/.git*" | cut -d "/" -f6- | fzf) || echo error
-    end
-end
